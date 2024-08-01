b2f32dd84419f8bbf3183060058751e29fa53803
rhalfaf <radek.halfaf@gmail.com>
2024-08-01 20:38:30 +0200

    first commit

---
+++ receipt-upload-app/src/main/java/pl/softr/receipt_upload_app/ReceiptUploadAppApplication.java
@@ -0,0 +1,13 @@
+package pl.softr.receipt_upload_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptUploadAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptUploadAppApplication.class, args);
+	}
+
+}
---
+++ receipt-upload-app/src/test/java/pl/softr/receipt_upload_app/ReceiptUploadAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.softr.receipt_upload_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptUploadAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
